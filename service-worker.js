a5b0c8cfad1fe74b6cfa1d8c95dfca8d2936b3fc
yurukkerem700-alt <yurukkerem700@gmail.com>
2026-09-11 23:09:19 +0300

    "PWA eklendi"

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'gokboru-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
