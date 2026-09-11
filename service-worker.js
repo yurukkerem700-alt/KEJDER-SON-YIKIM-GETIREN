3cdd6ddf0da49cd189f2526ed1631018d2e951f4
yurukkerem700-alt <yurukkerem700@gmail.com>
2026-09-12 00:16:00 +0300

    "Mobil kontroller ve PWA ayarları eklendi"

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'gokboru-v1';
-const assets = [
+const CACHE_NAME = 'boru-oyun-v1';
+const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
@@ -9,16 +9,19 @@ const assets = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+        // Önbellekte varsa onu kullan, yoksa internetten çek
+        return response || fetch(event.request);
+      })
   );
 });
